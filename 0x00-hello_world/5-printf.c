db6bf2290a5bb042f1b50046cf2af2907770da3c
DEWINTHONE <dewinthonetshitenge@gmail.com>
2023-03-19 17:04:49 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - This prints a text
- * Return: if succcess it'll return to 0
+ * Return: it'll return to 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf(u"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
