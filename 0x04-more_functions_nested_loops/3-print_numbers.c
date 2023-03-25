d0afbc0476908c9742bb58d13015cc88374a4349
DEWINTHONE <dewinthonetshitenge@gmail.com>
2023-03-24 18:59:52 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - pints the numbers 0 through 9
+ * Return: always 0.
+ */
+
+void print_number(void)
+{
+	int c;
+
+	for (c = 0; c < 9; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
