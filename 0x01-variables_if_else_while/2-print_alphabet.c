1139978b17574e32091572bea7606a45004fd830
DEWINTHONE <dewinthonetshitenge@gmail.com>
2023-04-13 12:38:36 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: will return back to 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (f = 'a'; i <= 'z'; i++)
+		putchar(i);
+		putchar ('\n');
+	return (0);
+}
